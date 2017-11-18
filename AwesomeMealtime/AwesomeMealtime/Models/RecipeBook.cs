b0c2cfd632c3d87340adad950ce1d51c9fe261a3
Damien Bevins <DBevins@student.neumont.edu>
2017-11-17 17:39:50 -0700

    Updates on RecipeBOok
    Created AddRecipe() and RemoveRecipe() Method

--- AwesomeMealtime/AwesomeMealtime/Models/RecipeBook.cs
+++ AwesomeMealtime/AwesomeMealtime/Models/RecipeBook.cs
@@ -8,5 +8,28 @@ namespace AwesomeMealtime.Models
 {
     public class RecipeBook
     {
+        public List<Recipe> Recipes { get; set; }
+
+        public void AddRecipe(Recipe rec)
+        {
+            if (rec != null)
+                Recipes.Add(rec);
+        }
+
+        public void RemoveRecipe(Recipe rec)
+        {
+            if(Recipes != null)
+            {
+                for (int i = 0; i < Recipes.Count; i++)
+                {
+                    if(Recipes[i].Name.Equals(rec.Name))
+                    {
+
+                    }
+                }
+            }
+
+            
+        }
     }
 }
