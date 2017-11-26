8848a7e258e85aaf90e290938cd8658486b4e1cf
Wesley Alnor <WAlnor@student.neumont.edu>
2017-11-25 23:53:12 -0700

    More still needs to be done with the interface but I think this is a good start, at least enough to be used.

---
+++ AwesomeMealtime/AwesomeMealtime/UI Interface Items/RecipeBTN.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using AwesomeMealtime.Models;
+using System.Windows;
+
+namespace AwesomeMealtime.UI_Interface_Items
+{
+    class RecipeBTN
+    {
+        public Recipe recipe;
+        public Button button;
+
+
+        public RecipeBTN(Recipe r)
+        {
+            recipe = r;
+
+            button = new Button()
+            {
+                Content = recipe.Name
+            };
+            button.Click += Click;
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            string toPrint = "";
+
+            MessageBox.Show(toPrint, recipe.Name);
+        }
+    }
+}
