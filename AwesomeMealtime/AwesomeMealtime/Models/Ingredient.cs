72daa909477b2172d6ca1f4ef95174b89c7e78c5
Damien Bevins <DBevins@student.neumont.edu>
2017-11-28 17:42:24 -0700

    Updates to conversions in Ingredient

--- AwesomeMealtime/AwesomeMealtime/Models/Ingredient.cs
+++ AwesomeMealtime/AwesomeMealtime/Models/Ingredient.cs
@@ -7,8 +7,12 @@ using System.Threading.Tasks;
 namespace AwesomeMealtime.Models
 {
     public class Ingredient
-        //Assigned to Invictus Valkyrius
+    //Assigned to Invictus Valkyrius
     {
+        //Create a conversion method, and store into a variable that is to be used.
+        //When setting its value, convert.
+        //When getting, convert back?
+
         public string Name { get; set; }
         public List<Quantity> Quantities { get; set; }
         public List<ExpDate> ExpirationDates { get; set; }
@@ -21,21 +25,83 @@ namespace AwesomeMealtime.Models
 
         public struct Quantity
         {
-            public double Qty { get; set; }
+            private double qty;
+
+            public double Qty
+            {
+                get
+                {
+                    return qty;
+                }
+                set
+                {
+                    
+                }
+            }
+
             public Measurements Msmt { get; set; }
+
+            double Convert(Measurements m)
+            {
+                if(m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+                else if (m == Measurements)
+                {
+
+                }
+
+                throw new NotImplementedException();
+            }
         }
 
         public enum Measurements
         {
             _,
+
+            //Imperial
             Cups,
             Gill,
-            Pint, 
+            Pint,
             Quart,
             Gallon,
             Peck,
             HalfBushel,
-            Bushel
+            Bushel,
+
+            //Metric
+
         }
+
+
     }
 }
