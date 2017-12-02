48bb73d857199a901c72e2890ebab89474f9d7b0
MKetchum <MKetchum@student.neumont.edu>
2017-12-01 18:09:45 -0700

    Updated the Filter and Sort methods

--- AwesomeMealtime/AwesomeMealtime/Models/Pantry.cs
+++ AwesomeMealtime/AwesomeMealtime/Models/Pantry.cs
@@ -22,9 +22,12 @@ namespace AwesomeMealtime.Models
         public Dictionary<string, Ingredient> FilterName(string filter) {
             //search for a specific ingredient by name
             Dictionary<string, Ingredient> filterIngredients = new Dictionary<string, Ingredient>();
+            filter = filter.ToLower();
+
             foreach(Ingredient ingredient in ingredients)
             {
-                if (filter.Equals(ingredient.Name))
+                string name = ingredient.Name.ToLower();
+                if (name.Contains(filter))
                 {
                     filterIngredients.Add(ingredient.Name, ingredient);
                 }
@@ -32,9 +35,16 @@ namespace AwesomeMealtime.Models
             return filterIngredients;
         }
 
-        public void Sort() {
-            //sorting the ingredients by alphebetical, expiration, etc.
+        public List<Ingredient> SortAlphabetical() {
+            //sort ingredients by alphabetical order
             ingredients.Sort((x, y) => string.Compare(x.Name, y.Name));
+            return ingredients;
+        }
+
+        public void SortReverseAlphabetical(){
+            //sort ingredients by reverse alphabetical order
+            ingredients.Sort((x, y) => string.Compare(x.Name, y.Name));
+            ingredients.Reverse();
         }
 
         public void Expiration_Warning() {
