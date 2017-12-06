39b9d95b87bb99f65c5669a0f1e86118ddc1f14c
Matthew Guernsey <MGuernsey@student.neumont.edu>
2017-12-05 18:18:27 -0700

    RecipeWindow

--- AwesomeMealtime/AwesomeMealtime/UI Interface Items/RecipeWindow.xaml.cs
+++ AwesomeMealtime/AwesomeMealtime/UI Interface Items/RecipeWindow.xaml.cs
@@ -27,5 +27,15 @@ namespace AwesomeMealtime.UI_Interface_Items
         private void Button_Click(object sender, RoutedEventArgs e)
         {
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
