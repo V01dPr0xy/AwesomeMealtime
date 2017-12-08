3de08e63f099d746d23251e0d6f75f2c01302ac0
bdorman <bdorman@student.neumont.edu>
2017-12-07 17:45:18 -0700

    Removed some useless methods

--- AwesomeMealtime/AwesomeMealtime/MainWindow.xaml.cs
+++ AwesomeMealtime/AwesomeMealtime/MainWindow.xaml.cs
@@ -20,12 +20,14 @@ namespace AwesomeMealtime
         Driver myDriver = new Driver();
         GridLength Biggie = new GridLength(20.0, GridUnitType.Star);
         GridLength Smalls = new GridLength(0.0, GridUnitType.Star);
-        public MainWindow()
+		private object sp_Expired;
+
+		public MainWindow()
         {
             InitializeComponent();
 			myDriver.Init();
 
-
+			Notifications();
 
             Closing += OnWindowClosing; //don't remove this
         }
@@ -40,7 +42,7 @@ namespace AwesomeMealtime
 				l = new Label();
 				l.Content = msg;
 				l.MouseLeftButtonDown += NotificationWarning_MouseLeftButtonDown;
-				//sp_ExSoon.Children.Add(l);
+				spl_Warning.Children.Add(l);
 			}
 
 			foreach (String msg in p.expRemovalMsg)
@@ -48,55 +50,22 @@ namespace AwesomeMealtime
 				l = new Label();
 				l.Content = msg;
 				l.MouseLeftButtonDown += NotificationDesposal_MouseLeftButtonDown;
-				//sp_ExWarnings.Children.Add(l);
+				spl_Expired.Children.Add(l);
 			}
 		}
-        private void ShowPantry_Click(object sender, RoutedEventArgs e)
-        {
-
-            LoadPantry();
-        }
         private void ShowRecipe_Click(object sender, RoutedEventArgs e)
         {
 
-            LoadRecipe();
+            
         }
         private void btn_Search_Click(object sender, RoutedEventArgs e)
         {
             //TO DO: add search functionality
             //Will need a check to see if we are in pantry or in Recipe book
             //string input = tb_Search.Text;
-            MessageBox.Show(input);
+            //MessageBox.Show(input);
             //TO DO: Something with input
         }
-      
-		/// <summary>
-        /// This will load the pantry data from the current pantry
-        /// to sp_Data, not to be confused with loading a new pantry
-        /// from a file
-        /// </summary>
-        private void LoadPantry()
-        {
-            //TO DO: Load Pantry from Pantry data
-        }
-
-        /// <summary>
-        /// This will load the Recipe book data from the current Recipe book
-        /// to sp_Data, not to be confused with loading a new Recipe book
-        /// from a file
-        /// </summary>
-        private void LoadRecipe()
-        {
-            //TO DO: Load Recipe book from Recipe book data
-            int i = 0;
-            foreach(Recipe rec in myDriver.Book.Recipes)
-            {
-                Label lbl = new Label();
-
-                i++;
-            }
-        }
-
         private void btn_RecipeAdd_Click(object sender, RoutedEventArgs e)
         {
             
