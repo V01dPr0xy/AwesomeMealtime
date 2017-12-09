a1e251c2a91156953fc71b24797d235c63142b01
Matthew Guernsey <MGuernsey@student.neumont.edu>
2017-12-08 17:59:42 -0700

    Add Recipe Button works

--- AwesomeMealtime/AwesomeMealtime/MainWindow.xaml.cs
+++ AwesomeMealtime/AwesomeMealtime/MainWindow.xaml.cs
@@ -33,6 +33,8 @@ namespace AwesomeMealtime
 		//Recipe Events
 		private void btn_RecipeAdd_Click(object sender, RoutedEventArgs e)
         {
+            RecipeWindow recwin = new RecipeWindow();
+            recwin.ShowDialog();
 		}
 		private void btn_RecipeRemove_Click(object sender, RoutedEventArgs e)
         {
